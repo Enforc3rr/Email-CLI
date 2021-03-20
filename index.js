bdaec2416cfae92561ca848e252be988cdb2adda
Prateek Kumar <prateekkumartiwari01@gmail.com>
2021-03-20 17:58:14 +0530

    Added Inquirer

---
+++ index.js
@@ -0,0 +1,34 @@
+const inquirer = require("inquirer");
+const puppeteer = require("puppeteer");
+
+inquirer
+.prompt(
+    [
+        {
+            type : "input",
+            message : "Enter Your Email",
+            name : "email"
+        },
+        {
+            type: "password",
+            message: "Enter Your Password",
+            name : "password",
+            mask : true
+        }
+    ]
+)
+.then(answers => {
+    const {email , password} = answers;
+    if(validateEmail(email))
+        console.log(email + "  " + password);
+    else
+        console.log(`Entered Email ID ${email} is not valid , Enter Other Email Address`);
+})
+.catch(err => {
+    console.log("Some Error occurred");
+});
+
+const validateEmail = email => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
